9789db1cd08f0964615768878f57a4dbd9755834
Frank Hecker <frank@frankhecker.com>
2021-05-14 17:51:37 -0400

    Support listing VPCs with same name, name with spaces

--- bloxone/oph_aws/list_vpc_aws.sh
+++ bloxone/oph_aws/list_vpc_aws.sh
@@ -12,7 +12,7 @@ usage() {
     echo >&2 "-1: print VPC ids one per line"
     echo >&2 "-l: also print VPC CIDR and Name tag (implies -1)"
     echo >&2 "<region>: AWS region"
-    echo >&2 "<vpc>: Name tag value or CIDR-format address of VPC"
+    echo >&2 "<vpc>: AWS VPC ID, CIDR-format address, or Name tag value"
     exit 1
 }
 
@@ -25,6 +25,7 @@ source "${HOME}"/.aws/set-aws-variables.sh
 one_per_line=false
 long_listing=false
 # Default value of REGION comes from set-aws-variable.sh.
+vpc=
 while getopts "1lr:" arg; do
     case "${arg}" in
 	1) one_per_line=true ;;
@@ -35,18 +36,17 @@ while getopts "1lr:" arg; do
 done
 shift `expr ${OPTIND} - 1`
 
-# Get VPC name/address argument if specified.
-# TODO: Make sure everything works if the VPC name contains spaces.
+# Get VPC ID/address/name if specified.
+# NOTE: The name may contain spaces if quoted on the command line.
 [ $# -gt 1 ] && usage
 vpc="$1"
 
 # Check to see if the region was specified incorrectly.
 [ -z "${REGION}" -o "${REGION}" = "-1" -o "${REGION}" = "-l" ] && usage
 
-# If no <vpc> argument, list all VPCs, displaying one per line and
-# with extra info if specified. Otherwise look for the specified VPC
-# by name or CIDR address and display extra info if specified.
-# TODO: Deal with the case where multiple VPCs have the same name.
+# If no <vpc> argument then list all VPCs, optionally displaying one
+# per line and extra info. Otherwise look for the specified VPC(s) by
+# ID, CIDR address, and name, optionally displaying extra info.
 if [ -z "${vpc}" ]; then
     if [ "${long_listing}" = true ]; then
 	aws ec2 describe-vpcs --no-paginate --output text \
@@ -60,30 +60,36 @@ if [ -z "${vpc}" ]; then
     else
 	aws ec2 describe-vpcs --no-paginate --output text \
 	    --region "${REGION}" \
-	    --query 'Vpcs[].VpcId'
+	    --query 'Vpcs[].VpcId' \
+	    | tr '\t' ' '
     fi
 else
-    # Look for the VPC first by CIDR-format address and then by name.
-    vpc_id=`aws ec2 describe-vpcs --no-paginate --output text \
-        --region "${REGION}" \
-	--filters "Name=cidr,Values=${vpc}" \
-	--query 'Vpcs[].VpcId'`
-    if [ -z "${vpc_id}" ]; then
-	vpc_id=`aws ec2 describe-vpcs --no-paginate --output text \
-	    --region "${REGION}" \
-            --filters "Name=tag:Name,Values=${vpc}" \
+    # Try to find the VPC by ID, CIDR-format address, or name.
+    # NOTE: A search by name may return multiple VPC IDs.
+    for vpc_designator in vpc-id cidr tag:Name; do
+	vpc_ids=`aws ec2 describe-vpcs --no-paginate --output text \
+            --region "${REGION}" \
+            --filters "Name=${vpc_designator},Values=${vpc}" \
             --query 'Vpcs[].VpcId'`
-	if [ -z "${vpc_id}" ]; then
-            echo >&2 "${fn}: ${vpc} not found"
-            exit 1
-	fi
+        [ ! -z "${vpc_ids}" ] && break
+    done
+    if [ -z "${vpc_ids}" ]; then
+        echo >&2 "${fn}: ${vpc} not found"
+        exit 1
     fi
+
+    # Handle the case of multiple IDs based on the display options.
     if [ "${long_listing}" = true ]; then
+	# NOTE: --vpc-ids allows multiple VPC IDs to be specified.
 	aws ec2 describe-vpcs --no-paginate --output text \
 	    --region "${REGION}" \
-	    --vpc-ids "${vpc_id}" \
+	    --vpc-ids ${vpc_ids} \
 	    --query 'Vpcs[].[VpcId, CidrBlock, (Tags[?Key==`Name`].Value)[0]]'
+    elif [ "${one_per_line}" = true ]; then
+	for vpc_id in ${vpc_ids}; do
+	    echo "${vpc_id}"
+	done
     else
-	echo "${vpc_id}"
+	echo ${vpc_ids}
     fi
 fi

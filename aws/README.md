9ffae326cc6d7549a2596194cc7358eda0e3e69f
Frank Hecker <frank@frankhecker.com>
2021-05-17 16:41:03 -0400

    Add script to list Internet gateways

--- aws/README.md
+++ aws/README.md
@@ -10,52 +10,61 @@ Utility scripts for working with AWS using the AWS CLI.
 * `create_subnet_aws.sh`: Create a subnet and optionally give it a name
 * `delete_vpc_aws.sh`: Delete a VPC by ID, address, or name.
 * `delete_subnet_aws.sh`: Delete a subnet by ID, address, or name.
+* `list_igw_aws.sh`: List Internet gateway IDs and (optionally) their
+  names and the VPCs they're attached to.
 
 Examples of how to use these scripts:
 
     # List VPC IDs in us-east-2 region, one per line
-    sh list_vpc_aws.sh -r us-east-2 -1
+    list_vpc_aws.sh -r us-east-2 -1
 
     # List info for VPC in default region with address 10.192.0.0/20
-    sh list_vpc_aws.sh -l 10.192.0.0/20
+    list_vpc_aws.sh -l 10.192.0.0/20
 
     # List VPC ID(s) for VPC(s) with name tag value "My VPC"
-    sh list_vpc_aws.sh "My VPC"
+    list_vpc_aws.sh "My VPC"
 
     # List info for all subnets in us-east-2 region
-    sh list_subnet_aws.sh -r us-east-2 -l
+    list_subnet_aws.sh -r us-east-2 -l
 
     # List info for subnet(s) in VPC 10.192.0.0/20 with name "My Subnet"
-    sh list_vpc_aws.sh -l -v 10.192.0.0/20 "My Subnet"
+    list_vpc_aws.sh -l -v 10.192.0.0/20 "My Subnet"
 
     # List subnet IDs in VPC vpc-020f5bab08c0d3f43 in region us-west-1
-    sh list_vpc_aws.sh -v vpc-020f5bab08c0d3f43 -r us-west-1
+    list_vpc_aws.sh -v vpc-020f5bab08c0d3f43 -r us-west-1
 
     # Create a VPC 10.192.0.0/20 in default region
-    sh create_vpc_aws.sh 10.192.0.0/20
+    create_vpc_aws.sh 10.192.0.0/20
 
     # Create a VPC 10.128.0.0/20 in us-west-2 region with name "My VPC"
-    sh create_vpc_aws.sh -r us-west-2 10.128.0.0/20 "My VPC"
+    create_vpc_aws.sh -r us-west-2 10.128.0.0/20 "My VPC"
 
     # Delete the VPC 10.192.0.0/20 in default region
-    sh delete_vpc_aws.sh 10.192.0.0/20
+    delete_vpc_aws.sh 10.192.0.0/20
 
     # Delete the VPC in us-west-2 region with name "My VPC"
-    sh delete_vpc_aws.sh -r us-west-2 "My VPC"
+    delete_vpc_aws.sh -r us-west-2 "My VPC"
 
     # Delete the VPC vpc-020f5bab08c0d3f43 in us-west-1 region
-    sh delete_vpc_aws.sh -r us-west-1 vpc-020f5bab08c0d3f43
+    delete_vpc_aws.sh -r us-west-1 vpc-020f5bab08c0d3f43
 
     # Create a subnet 10.192.1.0/24 in "My VPC" with name "My Subnet"
-    sh create_vpc_aws.sh "My VPC" 10.192.1.0/24 "My Subnet"
+    create_vpc_aws.sh "My VPC" 10.192.1.0/24 "My Subnet"
 
     # Delete the subnet 10.128.1.0/24 in us-west-1 region
-    sh delete_vpc_aws.sh -r us-west-1 10.128.1.0/24
+    delete_vpc_aws.sh -r us-west-1 10.128.1.0/24
 
-As implied above, the scripts allow VPCs and subnets to be identified
-by ID, address, or name (i.e., the value of the "Name" tag). The
-scripts properly handle cases where the name contains spaces or the
-name or address are duplicated across multiple VPCs or subnets.
+    # List info about Internet gateways in the us-east-2 region.
+    list_igw_aws.sh -l -r us-east-2
+
+    # List ID of Internet gateway attached to VPC 10.192.16.0/20.
+    list_igw_aws.sh -v 10.192.16.0/20
+
+As implied above, the scripts allow VPCs, subnets, and gateways to be
+identified by ID, name (i.e., the value of the "Name" tag), or address
+(if applicable). The scripts properly handle cases where the name
+contains spaces or the name or (if applicable) address are duplicated
+across multiple VPCs, subnets, or gateways.
 
 There are also some test scripts to verify proper functioning of the
 above scripts.
---
+++ aws/list_igw_aws.sh
@@ -0,0 +1,206 @@
+#!/bin/sh
+# list_igw_aws.sh: List internet gateway(s) in an AWS region.
+
+# Filename of script and the directory in its path as invoked.
+# NOTE: If script is invoked by filename only then ${dir} will be '.'.
+fn=`basename $0`
+dir=`dirname $0`
+
+# Print usage information if needed.
+usage() {
+    echo >&2 "Usage: ${fn} [-q] [-1] [-l] [-r <region>] [-v vpc] [<igw>]"
+    echo >&2 "-q: run quietly without unneeded messages"
+    echo >&2 "-1: display IGW ids one per line"
+    echo >&2 "-l: also display IGW's VPC (if any) and name (implies -1)"
+    echo >&2 "-r <region>: AWS region to search"
+    echo >&2 "-v <vpc>: VPC to look for an attached IGW"
+    echo >&2 "<igw>: internet gateway ID, address, or name to list"
+    exit 1
+}
+
+# Get added variables needed for AWS access, including default region.
+# NOTE: Credentials should be in the standard AWS-specified locations.
+# TODO: Remove the need for this extra file if possible.
+source "${HOME}"/.aws/set-aws-variables.sh
+
+# Check and extract optional arguments.
+quiet=false
+one_per_line=false
+long_listing=false
+# REGION=(default value comes from set-aws-variable.sh)
+vpc=
+while getopts "q1lar:v:" arg; do
+    case "${arg}" in
+        q) quiet=true ;;
+        1) one_per_line=true ;;
+        l) long_listing=true ;;
+        r) REGION="${OPTARG}" ;;
+        v) vpc="${OPTARG}" ;;
+        *) usage ;;
+    esac
+done
+shift `expr ${OPTIND} - 1`
+
+# Get internet gateway ID/name argument if specified.
+# NOTE: The name may contain spaces if quoted on the command line.
+[ $# -gt 1 ] && usage
+igw="$1"
+
+# Check to see if the region or VPC were specified incorrectly.
+[ -z "${REGION}" ] && usage
+case "${REGION}" in
+    -q|-1|-l|-v)
+        echo >&2 "${fn}: -r option missing region"
+        usage
+        ;;
+esac
+case "${vpc}" in
+    -q|-1|-l|-r)
+        echo >&2 "${fn}: -v option missing VPC"
+        usage
+        ;;
+esac
+
+# If a VPC was specified, find it by ID, address, or name.
+# NOTE: A search by name or address may return multiple VPC IDs.
+if [ -z "${vpc}" ]; then
+    vpc_ids=
+else
+    for vpc_designator in vpc-id cidr tag:Name; do
+        vpc_ids=`aws ec2 describe-vpcs --no-paginate --output text \
+            --region "${REGION}" \
+            --filters "Name=${vpc_designator},Values=${vpc}" \
+            --query 'Vpcs[].VpcId'`
+        [ ! -z "${vpc_ids}" ] && break
+    done
+    if [ -z "${vpc_ids}" ]; then
+        [ "${quiet}" = false ] && echo >&2 "${fn}: ${vpc} not found"
+        exit 1
+    fi
+fi
+
+# If no <igw> argument then list all internet gateways, optionally
+# displaying one per line and with extra info. Otherwise look for the
+# specified gateway(s) by ID and name, optionally displaying extra
+# info. In either event handle the case where the search is done in
+# one or more VPCs.
+if [ -z "${igw}" ]; then
+    if [ "${long_listing}" = true ]; then
+        if [ -z "${vpc_ids}" ]; then
+            # NOTE: To handle the case where gateway is not attached
+            # to a VPC, we convert the returned attachments list to a
+            # string and then use sed to produce the desired output.
+            aws ec2 describe-internet-gateways \
+                --no-paginate --output text \
+                --region "${REGION}" \
+                --query 'InternetGateways[].[InternetGatewayId, to_string(Attachments[].VpcId), (Tags[?Key==`Name`].Value)[0]]' \
+            | sed -e 's/\[\]/None/' -e 's/\["//' -e 's/"\]//'
+        else
+            for vpc_id in ${vpc_ids}; do
+                aws ec2 describe-internet-gateways \
+                    --no-paginate --output text \
+                    --region "${REGION}" \
+                    --filters "Name=attachment.vpc-id,Values=${vpc_id}" \
+                    --query 'InternetGateways[].[InternetGatewayId, to_string(Attachments[].VpcId), (Tags[?Key==`Name`].Value)[0]]' \
+                | sed -e 's/\[\]/None/' -e 's/\["//' -e 's/"\]//'
+            done
+        fi
+    elif [ "${one_per_line}" = true ]; then
+        if [ -z "${vpc_ids}" ]; then
+            aws ec2 describe-internet-gateways \
+                --no-paginate --output text \
+                --region "${REGION}" \
+                --query 'InternetGateways[].InternetGatewayId' \
+            | tr '\t' '\n'
+        else
+            for vpc_id in ${vpc_ids}; do
+                aws ec2 describe-internet-gateways \
+                    --no-paginate --output text \
+                    --region "${REGION}" \
+                    --filters "Name=attachment.vpc-id,Values=${vpc_id}" \
+                    --query 'InternetGateways[].InternetGatewayId' \
+                | tr '\t' '\n'
+            done
+        fi
+    else
+        # List Internet gateway IDs on same line.
+        if [ -z "${vpc_ids}" ]; then
+            # List all Internet gateway IDs.
+            aws ec2 describe-internet-gateways \
+                --no-paginate --output text \
+                --region "${REGION}" \
+                --query 'InternetGateways[].InternetGatewayId' \
+            | tr '\t' ' '
+        else
+            # List gateway ID(s) attached to specified VPC(s).
+            igw_ids=
+            for vpc_id in ${vpc_ids}; do
+                new_igw_ids=`aws ec2 describe-internet-gateways \
+                    --no-paginate --output text \
+                    --region "${REGION}" \
+                    --filters "Name=attachment.vpc-id,Values=${vpc_id}" \
+                    --query 'InternetGateways[].InternetGatewayId' \
+                | tr '\t' ' '`
+                if [ -z "$igw_ids" ]; then
+                    igw_ids="${new_igw_ids}"
+                else
+                    igw_ids="${igw_ids} ${new_igw_ids}"
+                fi
+            done
+            echo ${igw_ids}
+        fi
+    fi
+else
+    # Try to find the Internet gateway by ID or name.
+    # NOTE: A search by name may return multiple gateway IDs.
+    if [ -z "${vpc_ids}" ]; then
+        for igw_designator in internet-gateway-id tag:Name; do
+            igw_ids=`aws ec2 describe-internet-gateways \
+                --no-paginate --output text \
+                --region "${REGION}" \
+                --filters "Name=${igw_designator},Values=${igw}" \
+                --query 'InternetGateways[].InternetGatewayId'`
+            [ ! -z "${igw_ids}" ] && break
+        done
+    else
+        igw_ids=
+        for vpc_id in ${vpc_ids}; do
+            for igw_designator in internet-gateway-id tag:Name; do
+                new_igw_ids=`aws ec2 describe-internet-gateways \
+                    --no-paginate --output text \
+                    --region "${REGION}" \
+                    --filters \
+                        "Name=${igw_designator},Values=${igw}" \
+                        "Name=attachment.vpc-id,Values=${vpc_id}" \
+                    --query 'InternetGateways[].InternetGatewayId'`
+                [ ! -z "${new_igw_ids}" ] && break
+            done
+            if [ -z "$igw_ids" ]; then
+                igw_ids="${new_igw_ids}"
+            else
+                igw_ids="${igw_ids} ${new_igw_ids}"
+            fi
+        done
+    fi
+    if [ -z "${igw_ids}" ]; then
+        [ "${quiet}" = false ] && echo >&2 "${fn}: ${igw} not found"
+        exit 1
+    fi
+
+    # Handle the case of multiple IDs based on the display options.
+    if [ "${long_listing}" = true ]; then
+        # NOTE: --internet-gateway-ids allows multiple IDs to be specified.
+        aws ec2 describe-internet-gateways \
+            --no-paginate --output text \
+            --region "${REGION}" \
+            --internet-gateway-ids ${igw_ids} \
+            --query 'InternetGateways[].[InternetGatewayId, to_string(Attachments[].VpcId), (Tags[?Key==`Name`].Value)[0]]' \
+        | sed -e 's/\[\]/None/' -e 's/\["//' -e 's/"\]//'
+    elif [ "${one_per_line}" = true ]; then
+        for igw_id in ${igw_ids}; do
+            echo "${igw_id}"
+        done
+    else
+        echo ${igw_ids}
+    fi
+fi
